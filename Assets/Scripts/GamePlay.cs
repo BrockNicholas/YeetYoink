c9484d7083f402b7c6b48222cb6d607c7d3d8878
BrockW <brock@Brocks-MacBook-Pro.local>
2021-01-29 16:13:58 -0500

    Probably as far as I'll get in one session

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameRunner : MonoBehaviour
+public class GamePlay : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
