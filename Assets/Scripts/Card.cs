6d5f9487cfcf17aac628bca3e1597682a80d2802
Brock <33275496+BrockNicholas@users.noreply.github.com>
2021-01-31 22:55:09 -0500

    First version of throwing cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private bool IsPlaying = false;
+    private Vector3 WhereToGo;
+    private int DebugCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x == WhereToGo.x)
+        {
+            IsPlaying = false;
+        }
+
+        if (IsPlaying)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, WhereToGo, .1f);
+            transform.Rotate(0, 0, 30);
+            DebugCount++;
+            Debug.Log(DebugCount);
+        }
+    }
+
+    public void Play(Vector3 whereToGo)
+    {
+        WhereToGo = whereToGo;
+        IsPlaying = true;
+    }
+}
--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -10,6 +10,8 @@ public class GamePlay : MonoBehaviour
     public GameObject MainCard;
     public GameObject P2Card;
 
+    public float Speed;
+
     private Vector3 P1DeckCoord;
     private Vector3 P1PlayCoord;
     private Vector3 MainDeckCoord;
@@ -27,12 +29,46 @@ public class GamePlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetStartingPositions();
-    }
+        SetStartingPositions();        
+
+    }    
 
     // Update is called once per frame
     void Update()
     {
+        GetDebugInputs(); // Debug
+    }
+
+    void GetDebugInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayCard(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayCard(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MainCard.GetComponent<Card>().Play(MainPlayCoord);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            MoveToStartingPositions();
+        }
+    }
+
+    void PlayCard(bool p1)
+    {
+        if (p1)
+        {
+            P1Card.GetComponent<Card>().Play(P1PlayCoord);
+        }
+        else
+        {
+            P2Card.GetComponent<Card>().Play(P2PlayCoord);
+        }
         
     }
 
@@ -43,17 +79,20 @@ public class GamePlay : MonoBehaviour
 
     void MoveToStartingPositions()
     {
-
+        P1Card.transform.position = P1DeckCoord;
+        MainCard.transform.position = MainDeckCoord;
+        P2Card.transform.position = P2DeckCoord;
     }
 
     void SetStartingPositions()
     {
         P1DeckCoord = P1Card.transform.position;
-        P1PlayCoord = Camera.main.WorldToScreenPoint(GameObject.Find("P1PlayCoord").transform.position);        
+        P1PlayCoord = GameObject.Find("P1PlayCoord").transform.position;
         MainDeckCoord = MainCard.transform.position;
         MainPlayCoord = GameObject.Find("GameManager").transform.position;
         P2DeckCoord = P2Card.transform.position;
         P2PlayCoord = GameObject.Find("P2PlayCoord").transform.position;
 
     }
+    
 }
